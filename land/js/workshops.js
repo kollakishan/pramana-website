ff79eb070628ba1abd68064f2df5ddd71f60d8f3
leela kishan kolla <kollakishan@gmail.com>
2019-10-15 22:47:12 -0500

    Create workshops.js

---
+++ land/js/workshops.js
@@ -0,0 +1,201 @@
+function af1()
+{
+document.images[0].src="images/11.png"
+}
+
+function af2()
+{
+document.images[1].src="images/22.png" 
+}
+
+function af3()
+{
+document.images[2].src="images/33.png" 
+}
+
+function af4()
+{
+document.images[3].src="images/44.png" 
+}
+
+function af5()
+{
+document.images[4].src="images/55.png" 
+}
+
+function af6()
+{
+document.images[5].src="images/66.png" 
+}
+
+function af7()
+{
+document.images[6].src="images/77.png" 
+}
+
+function af8()
+{
+document.images[7].src="images/88.png" 
+}
+
+function af9()
+{
+document.images[8].src="images/99.png" 
+}
+
+function af10()
+{
+document.images[9].src="images/p11.png" 
+}
+
+function af1a()
+{
+document.images[10].src="images/e11a.png" 
+}
+
+function vf1()
+{
+document.images[11].src="images/v11.png"
+}
+
+function vf2()
+{
+document.images[12].src="images/v22.png" 
+}
+
+function vf3()
+{
+document.images[13].src="images/v33.png" 
+}
+
+function vf4()
+{
+document.images[14].src="images/v44.png" 
+}
+
+function vf5()
+{
+document.images[15].src="images/v55.png" 
+}
+
+function vf6()
+{
+document.images[16].src="images/v66.png" 
+}
+
+function vf7()
+{
+document.images[17].src="images/v77.png" 
+}
+
+function vf8()
+{
+document.images[18].src="images/v88.png" 
+}
+
+
+
+
+
+
+
+
+
+
+
+function af11()
+{
+document.images[0].src="images/1a.png"
+}
+
+function af22()
+{
+document.images[1].src="images/2.png" 
+}
+
+function af33()
+{
+document.images[2].src="images/3.png" 
+}
+
+function af44()
+{
+document.images[3].src="images/4.png" 
+}
+
+function af55()
+{
+document.images[4].src="images/5.png" 
+}
+
+function af66()
+{
+document.images[5].src="images/6.png" 
+}
+
+function af77()
+{
+document.images[6].src="images/7.png" 
+}
+
+function af88()
+{
+document.images[7].src="images/8.png" 
+}
+
+function af99()
+{
+document.images[8].src="images/9.png" 
+}
+
+function af1010()
+{
+document.images[9].src="images/p1.png" 
+}
+
+function af11aa()
+{
+document.images[10].src="images/e1.png" 
+}
+
+
+function vf11()
+{
+document.images[11].src="images/v1.png"
+}
+
+function vf22()
+{
+document.images[12].src="images/v2.png" 
+}
+
+function vf33()
+{
+document.images[13].src="images/v3.png" 
+}
+
+function vf44()
+{
+document.images[14].src="images/v4.png" 
+}
+
+function vf55()
+{
+document.images[15].src="images/v5.png" 
+}
+
+function vf66()
+{
+document.images[16].src="images/v6.png" 
+}
+
+function vf77()
+{
+document.images[17].src="images/v7.png" 
+}
+
+function vf88()
+{
+document.images[18].src="images/v8.png" 
+}
+
