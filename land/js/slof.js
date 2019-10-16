518b8da66de339127963f2bac01343f6a144a4d2
leela kishan kolla <kollakishan@gmail.com>
2019-10-15 22:47:21 -0500

    Create slof.js

---
+++ land/js/slof.js
@@ -0,0 +1,63 @@
+function af1()
+{
+document.images[0].src="images/Mobile.png"
+}
+function af2()
+{
+document.images[0].src="images/Cycle.png"
+}
+function af3()
+{
+document.images[0].src="images/Podium.png"
+}
+function af4()
+{
+document.images[0].src="images/MBA.png"
+}
+function af5()
+{
+document.images[0].src="images/Guitarist.png"
+}
+function af6()
+{
+document.images[0].src="images/Singer.png"
+}
+function af7()
+{
+document.images[0].src="images/leaf.png"
+}
+
+
+
+function af11()
+{
+document.images[0].src="images/transp.png"
+}
+function af22()
+{
+document.images[0].src="images/transp.png"
+}
+function af33()
+{
+document.images[0].src="images/transp.png"
+}
+function af44()
+{
+document.images[0].src="images/transp.png"
+}
+function af55()
+{
+document.images[0].src="images/transp.png"
+}
+function af66()
+{
+document.images[0].src="images/transp.png"
+}
+function af66()
+{
+document.images[0].src="images/transp.png"
+}
+function af77()
+{
+document.images[0].src="images/transp.png"
+}
